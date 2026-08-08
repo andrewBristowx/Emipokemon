39c517fcb73738cef58f6880f236e77b8e14dc8c
andrewBristowx <138131314+andrewBristowx@users.noreply.github.com>
2026-08-08 13:05:51 -0500

    Bump phase 2 test build to alpha.2

--- src/main/java/com/emipokemon/Emipokemon.java
+++ src/main/java/com/emipokemon/Emipokemon.java
@@ -16,7 +16,7 @@ import org.slf4j.LoggerFactory;
 
 public final class Emipokemon implements ModInitializer {
     public static final String MOD_ID = "emipokemon";
-    public static final String VERSION = "0.2.0-alpha.1";
+    public static final String VERSION = "0.2.0-alpha.2";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     private static final ConfigManager CONFIG_MANAGER = new ConfigManager();
